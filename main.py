617886828fd0f3a9ae01c279577da34fab91bad4
grlarsen <gregor.larsen@gmail.com>
2025-09-09 16:40:08 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Entry Point',
+    page_icon='❂'
+)
+
+st.write(
+    '**Welcome to init page**'
+)
+
+st.sidebar(
+    'Select page'
+)
