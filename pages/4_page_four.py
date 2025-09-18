f79ef2f5810c8184cd890fe53729655ff13d68fd
gsxbl <gregor.larsen@gmail.com>
2025-09-19 01:05:27 +0200

    these aren't the droids .gif

--- pages/4_page_four.py
+++ pages/4_page_four.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.header('Dummypage 4')
+class Page3:
+    def __init__(self):
+        st.set_page_config(layout='wide')
+
+        st.header("These aren't the droids you're looking for")
+        st.image(''.join(['https://media4.giphy.com/media/',
+                 'v1.Y2lkPTc5MGI3NjExa2s3eXN2cnFlc3pjZnkyMnU4Z',
+                 'm1laDZ1ZDlmZ2dhMXkzcWxrenlpZyZlcD12MV9pbnRlcm',
+                 '5hbF9naWZfYnlfaWQmY3Q9Zw/4560Nv2656Gv0Lvp9F/giphy.gif']),
+                 width='stretch')
+
+if __name__ == '__main__': 
+    Page3()
