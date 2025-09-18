a305d893385b9deb1718747ab6df18e3c33e8845
gsxbl <gregor.larsen@gmail.com>
2025-09-19 00:00:54 +0200

    page3 MVP

--- pages/3_page_three.py
+++ pages/3_page_three.py
@@ -1,3 +1,60 @@
 import streamlit as st
+import plotly.graph_objects as go
 
-st.header('Dummypage 3')
+from modules.fetch import csv_data
+
+class Page3:
+    def __init__(self):
+        st.set_page_config(layout='wide')
+        st.header('Dummypage 3')
+
+        self._df = csv_data(
+            'data/open-meteo-subset.csv', index_col=0,
+            parse_dates=['time'])
+        
+        self._get_months()
+    
+    def _get_months(self):
+        # Convert to year-month format
+        months = self._df.index.to_period("M")
+        # Unique sorted year-months
+        self._months = months.sort_values().unique()
+        
+        
+    def plot(self):
+        if self._column:
+            fig = go.Figure()
+
+            fig.add_trace(
+                go.Scatter(
+                    x=self._df.index,
+                    y=self._df[self._column],
+                    name=self._column, yaxis="y1"))
+            
+            st.plotly_chart(fig)
+
+    def slice_data(self):
+        start, stop = self._slice
+        self._df = self._df[
+            (self._df.index.to_period("M")>= start) &
+            (self._df.index.to_period("M") <= stop)
+            ]
+
+    # --- PAGE CONTENTS ---
+    def setup_contents(self):
+        self._column = st.selectbox('Columns', self._df.columns)
+        self._slice = st.select_slider(
+            "Select time range",
+            options=self._months,
+            value=(self._months[0], self._months[-1])
+        )
+        
+        
+    def run(self):
+        self.setup_contents()
+        self.slice_data()
+        self.plot()
+
+if __name__ == '__main__':
+    main = Page3()
+    main.run()
