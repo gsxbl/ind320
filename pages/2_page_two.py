e6e8fa7dc59ec5984b5febae132ad8cd4184cad7
gsxbl <gregor.larsen@gmail.com>
2025-09-18 21:47:57 +0200

    page two MVP

--- pages/2_page_two.py
+++ pages/2_page_two.py
@@ -1,9 +1,32 @@
 import streamlit as st
-import pandas as pd
+from modules.fetch import csv_data, agg_first_month
 
-st.set_page_config(layout='wide')
+class Main:
+    def __init__(self):
+        st.set_page_config(layout='wide')
+        st.header('Dummypage 2')
 
-st.header('Dummypage 2')
 
-df = pd.read_csv('data/open-meteo-subset.csv')
-st.dataframe(df)
+    # --- PAGE CONTENTS ---
+    def setup_contents(self):
+        # get the data, use custom function with caching
+        df = csv_data('data/open-meteo-subset.csv', index_col=0, parse_dates=['time'])
+
+        # aggreagte the first month
+        df = agg_first_month(df)
+
+        # # set configuration for linecharts within Dataframe
+        col_config = {
+            col : st.column_config.LineChartColumn(col)
+            for col in df.columns
+            }
+        
+        # render dataframe to frontend
+        st.dataframe(df, column_config=col_config)
+        
+    def run(self):
+        self.setup_contents()
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
