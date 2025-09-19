fc41254c17f5226cc286a66e130d7438a15c9899
gsxbl <gregor.larsen@gmail.com>
2025-09-19 12:27:06 +0200

    stacked column line charts horisontally

--- pages/2_page_two.py
+++ pages/2_page_two.py
@@ -6,23 +6,21 @@ class Page2:
         st.set_page_config(layout='wide')
         st.header('Dummypage 2')
 
+        self._df = csv_data('data/open-meteo-subset.csv', index_col=0, parse_dates=['time'])
 
     # --- PAGE CONTENTS ---
     def setup_contents(self):
         # get the data, use custom function with caching
-        df = csv_data('data/open-meteo-subset.csv', index_col=0, parse_dates=['time'])
 
         # aggreagte the first month
-        df = agg_first_month(df)
+        df = agg_first_month(self._df)
 
         # # set configuration for linecharts within Dataframe
-        col_config = {
-            col : st.column_config.LineChartColumn(col)
-            for col in df.columns
-            }
-        
-        # render dataframe to frontend
-        st.dataframe(df, column_config=col_config)
+        for col in df.columns:
+            col_cfg = st.column_config.LineChartColumn(col)
+            
+            # render to frontend
+            st.dataframe(df[col], column_config={col:col_cfg})
         
     def run(self):
         self.setup_contents()
