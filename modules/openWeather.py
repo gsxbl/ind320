2ed9f62a932de34966eaa858cfe37c217d24d57b
gsxbl <gregor.larsen@gmail.com>
2025-10-03 01:10:06 +0200

    forcast and location API calls are functional

--- modules/openWeather.py
+++ modules/openWeather.py
@@ -14,16 +14,28 @@ class APICall(ABC):
         self._units = 'metric'
         self._run = False
         self._resp = None
+        self._success = False
+    
+    @property
+    def success(self):
+        return self._success
 
     @abstractmethod 
     def setup_query(self, kind, **kwargs):
         pass
     
-    @abstractmethod
-    def search(self):
+    def call(self):
         if not self._run:
             return
         self._resp = re.get(self._url)
+        if self._resp.status_code == 200:
+            self._success = True
+
+    def __call__(self, *args, **kwds):
+        if isinstance(self._resp.json(), list):
+            return self._resp.json()[0]
+        else:
+            return self._resp.json()
 
 
 class GeoCode(APICall):
@@ -54,9 +66,6 @@ class GeoCode(APICall):
         # enable api call
         self._run = True
     
-    def search(self):
-        return super().search()
-    
 
 class Forcast(APICall):
     '''Special class for forcast calls'''
@@ -64,26 +73,39 @@ class Forcast(APICall):
         super().__init__(apiKey)
         self._prefix = 'data/2.5/forecast?'
 
-    def setup_query(self, kind, **kwargs):
+    def setup_query(self, **kwargs):
         lat = kwargs.get('lat', None)
-        lon = kwargs.get('lon`', None)
+        lon = kwargs.get('lon', None)
 
+        # don't call the API without geolocation
         if not lat and lon:
             return
         
         self._url = self._BASEURL + self._prefix + \
-            f'lat={lat}&lon={lon}' + f'units={self._units}' + \
+            f'lat={lat}&lon={lon}' + f'&units={self._units}' + \
                 f'&appid={self._APIkey}'
 
         self._run = True
-    
-    def search(self):
-        return super().search()
+
 
 class Weather:
+    '''Wrapper class to hold all calls'''
     def __init__(self, apiKey):
         self._geoCode = GeoCode(apiKey)
-        # self._forcast = Forcast(apiKey)
+        self._forcast = Forcast(apiKey)
+
+    def location(self, kind, **kwargs):
+        self.geoCode.setup_query(kind, **kwargs)
+        self.geoCode.call()
+
+    def upcoming(self):
+        if not self.geoCode.success:
+            st.write('forcast not called')
+            return
+        self.forcast.setup_query(**self.geoCode())
+        st.write(self.forcast._url)
+        # self.forcast.call()
+        # return self.forcast()
 
     @property
     def geoCode(self):
