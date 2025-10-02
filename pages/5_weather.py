55fc086ae94fea8a6be3f8fda428c402f5a05ee6
gsxbl <gregor.larsen@gmail.com>
2025-10-02 23:39:47 +0200

    initial weather forcast page

---
+++ pages/5_weather.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from modules.openWeather import Weather
+
+class Page5:
+    
+    def __init__(self):
+        # general page setup
+        st.set_page_config(layout='wide')
+        st.header('[dev] Weather app')
+
+        # API wrappers
+        self._APIkey = st.secrets['openWeather']['apiKey']
+        self._Weather = Weather(self._APIkey)
+        self._kwargs = {}
+
+    def _setup_contents(self):
+        self._mode = st.pills('Select search mode',
+                 ['name', 'zip'])
+    
+        if self._mode == 'name':
+            self._kwargs['city'] = st.text_input('Location name:')
+
+        elif self._mode == 'zip':
+            self._kwargs['zip'] = st.text_input('Zip Code:')
+            self._kwargs['cc'] = st.text_input('Country code:')
+
+    def get_geolocation(self):
+        self._Weather.geoCode.setup_query(
+            self._mode, **self._kwargs
+            )
+        self._Weather.geoCode.search()
+
+
+    def run(self):
+        self._setup_contents()
+        self.get_geolocation()
+
+if __name__ == '__main__':
+    page = Page5()
+    page.run()
