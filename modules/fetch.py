087f333e6f401df8bc709a95e4d464952abbceed
gsxbl <gregor.larsen@gmail.com>
2025-10-26 11:53:46 +0100

    refactored agg_month() to aggregate specified month

--- modules/fetch.py
+++ modules/fetch.py
@@ -11,16 +11,15 @@ def csv_data(path:str, **kwargs):
 
 
 ### LINE CHART COLUMN HELPER ###
-@st.cache_data
-def agg_first_month(df:pd.DataFrame):
-    minyear = df.index.year.min()
-    minmonth = df.loc[f"{minyear}"].index.month.min()
+def agg_month(df:pd.DataFrame, **kwargs):
+    minmonth = kwargs.get('month', df.index.month.min())
+    minyear = kwargs.get('year', df.index.year.min())
     df = df.loc[f"{minyear}-{minmonth:02d}"]
     df = df.groupby(df.index.month).agg(list)
+    df.index.name = 'Month'
     return df
 
 ### EXTRACT MONTHS HELPER ###
-@st.cache_data
 def months(timestamp):
     return pd.Series(timestamp).dt.to_period('M').unique()
 
