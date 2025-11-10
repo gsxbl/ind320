be19757059d54c32961a8b358198c87099a0a2de
gsxbl <gregor.larsen@gmail.com>
2025-11-10 08:24:33 +0100

    added elSpotOmraade 'id'

--- modules/geo.py
+++ modules/geo.py
@@ -6,6 +6,7 @@ class GeoPos:
         self._locations = {
             "NO1": {
                 'city': "Oslo",
+                'nve_code': 6,
                 'location': {
                     "latitude": 59.91,
                     "longitude": 10.75
@@ -13,6 +14,7 @@ class GeoPos:
             },
             "NO2": {
                 'city': "Kristiansand",
+                'nve_code': 7,
                 'location': {
                     "latitude": 58.15,
                     "longitude": 8.00
@@ -20,6 +22,7 @@ class GeoPos:
             },
             "NO3": {
                 'city': "Trondheim",
+                'nve_code': 7,
                 'location': {
                     "latitude": 63.43,
                     "longitude": 10.40
@@ -27,6 +30,7 @@ class GeoPos:
             },
             "NO4": {
                 'city': "Tromsø",
+                'nve_code': 8,
                 'location': {
                     "latitude": 69.65,
                     "longitude": 18.96
@@ -34,6 +38,7 @@ class GeoPos:
             },
             "NO5": {
                 'city': "Bergen",
+                'nve_code': 9,
                 'location': {
                     "latitude": 60.39,
                     "longitude": 5.32
@@ -41,11 +46,11 @@ class GeoPos:
             },
         }
 
-    def __call__(self, arg):
+    def __call__(self, area_code):
         '''Method to get lat and long from area code'''
-        loc = self._locations.get(arg)
+        loc = self._locations.get(area_code)
         return loc['location'] if loc else None
-    
+
     def city_name(self, area_code):
         '''Method to get city name from area code'''
         loc = self._locations.get(area_code)
